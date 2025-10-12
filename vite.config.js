c1bc9b5ef2660e42ce8906501167c0cc4a36bb9f
Pk13055 <Pk13055@users.noreply.github.com>
2025-10-13 01:03:27 +0530

    Convert Gatsby site to Vite + React boilerplate

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // For user/org GitHub Pages site (pk13055.github.io)
+})
